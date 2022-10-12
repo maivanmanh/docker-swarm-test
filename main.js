36536841c0eb93acc59ebe217367787020d651f5
Mai Van Manh <mvmanh@gmail.com>
2022-10-12 20:50:23 +0700

    test 2

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ code: 0, message: 'Everything is working fine!!', success: true });
+    res.json({ code: 0, message: 'OK', success: true });
 })
 
 app.post('/github', (req, res) => {
