d2afdf981f9b74b3e1fd7d49b800833d48c78d26
Mai Van Manh <mvmanh@gmail.com>
2022-10-12 20:52:35 +0700

    return hostname

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const express = require('express');
+var os = require("os");
+
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ code: 0, message: 'OK', success: true });
+    res.json({ code: 0, message: `This message was sent from: ` + os.hostname, success: true });
 })
 
 app.post('/github', (req, res) => {
