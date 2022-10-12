5a5fbb22c6e222be3e4be38442417784fcf42539
Mai Van Manh <mvmanh@gmail.com>
2022-10-12 19:48:57 +0700

    test hook

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ code: 0, message: 'Hello World!', success: true });
+    res.json({ code: 0, message: 'Github hook has been set', success: true });
 })
 
 app.post('/github', (req, res) => {
